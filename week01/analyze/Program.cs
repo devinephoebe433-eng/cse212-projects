f27a14d2e20ee1b9b2e991b25cd51878eb0a1e3e
Devine <devinephoebe433@gmail.com>
2026-03-13 12:10:24 +0300

    devine33

--- week01/analyze/Program.cs
+++ week01/analyze/Program.cs
@@ -1,8 +1,2 @@
-﻿Console.WriteLine("\n======================\nSorting\n======================");
-Sorting.Run();
-
-Console.WriteLine("\n======================\nStandardDeviation\n======================");
-StandardDeviation.Run();
-
-Console.WriteLine("\n======================\nSearch\n======================");
+﻿
 Search.Run();
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,33 +1,37 @@
 public static class Arrays
 {
-    /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
-    /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    public static double[] MultiplesOf(double number, int length)
+    // PLAN FOR MultiplesOf
+    // 1. Receive startNumber and count
+    // 2. Create a list to store multiples
+    // 3. Loop from 1 to count
+    // 4. Multiply startNumber by the loop number
+    // 5. Add result to the list
+    // 6. Return the list as an array
+    public static int[] MultiplesOf(int startNumber, int count)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        List<int> numbers = new List<int>();
 
-        return []; // replace this return statement with your own
+        for (int i = 1; i <= count; i++)
+        {
+            numbers.Add(startNumber * i);
+        }
+
+        return numbers.ToArray();
     }
 
-    /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
-    ///
-    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
-    /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
+    // PLAN FOR RotateRight
+    // 1. Save the last value
+    // 2. Move each element to the right
+    // 3. Put the last value at the first position
+    public static void RotateRight(int[] data)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        int last = data[data.Length - 1];
+
+        for (int i = data.Length - 1; i > 0; i--)
+        {
+            data[i] = data[i - 1];
+        }
+
+        data[0] = last;
     }
 }
