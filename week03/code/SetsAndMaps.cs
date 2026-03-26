a0f541e0fc89e24646719a8e82e819b92acb541a
Devine <devinephoebe433@gmail.com>
2026-03-26 14:40:13 +0300

    devine433

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,106 +1,196 @@
-using System.Text.Json;
+// ==========================================
+// W03: Sets and Maps Assignment
+// Name: Nabugobero Devine Phoebe
+// ==========================================
 
-public static class SetsAndMaps
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
 {
-    /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for returning all symmetric pairs of words.  
-    ///
-    /// For example, if words was: [am, at, ma, if, fi], we would return :
-    ///
-    /// ["am & ma", "if & fi"]
-    ///
-    /// The order of the array does not matter, nor does the order of the specific words in each string in the array.
-    /// at would not be returned because ta is not in the list of words.
-    ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be returned.
-    /// </summary>
-    /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
-    public static string[] FindPairs(string[] words)
+    static void Main()
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
-    }
+        // Problem 1 Test
+        Console.WriteLine("Problem 1:");
+        var pair = FindPairs(new int[] { 2, 7, 11, 15 }, 9);
+        Console.WriteLine(string.Join(", ", pair));
 
-    /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
-    /// </summary>
-    /// <param name="filename">The name of the file to read</param>
-    /// <returns>fixed array of divisors</returns>
-    public static Dictionary<string, int> SummarizeDegrees(string filename)
-    {
-        var degrees = new Dictionary<string, int>();
-        foreach (var line in File.ReadLines(filename))
+        // Problem 2 Test
+        Console.WriteLine("\nProblem 2:");
+        var summary = DegreeSummary(new int[] { 1, 2, 2, 3, 3, 3 });
+        foreach (var item in summary)
         {
-            var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            Console.WriteLine($"{item.Key}: {item.Value}");
         }
 
-        return degrees;
+        // Problem 3 Test
+        Console.WriteLine("\nProblem 3:");
+        var anagrams = GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+        foreach (var group in anagrams)
+        {
+            Console.WriteLine(string.Join(", ", group));
+        }
+
+        // Problem 4 Test
+        Console.WriteLine("\nProblem 4:");
+        int[,] maze = {
+            {0, 1, 0},
+            {0, 0, 0},
+            {1, 0, 1}
+        };
+        Console.WriteLine(CanExitMaze(maze, (0, 0), (1, 2)));
+
+        // Problem 5 Test
+        Console.WriteLine("\nProblem 5:");
+        var earthquakeData = new List<string> { "California", "Alaska", "California" };
+        var result = EarthquakeSummary(earthquakeData);
+        foreach (var item in result)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
     }
 
-    /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
-    /// Examples:
-    /// is_anagram("CAT","ACT") would return true
-    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
-    /// </summary>
-    public static bool IsAnagram(string word1, string word2)
+
+    // ------------------------------
+    // Problem 1: Finding Pairs
+    // ------------------------------
+    static int[] FindPairs(int[] numbers, int target)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
+        HashSet<int> seen = new HashSet<int>();
+
+        foreach (int num in numbers)
+        {
+            int needed = target - num;
+
+            if (seen.Contains(needed))
+            {
+                return new int[] { needed, num };
+            }
+
+            seen.Add(num);
+        }
+
+        return new int[] { };
+    }
+
+
+    // ------------------------------
+    // Problem 2: Degree Summary
+    // ------------------------------
+    static Dictionary<int, int> DegreeSummary(int[] numbers)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        foreach (int num in numbers)
+        {
+            if (map.ContainsKey(num))
+            {
+                map[num]++;
+            }
+            else
+            {
+                map[num] = 1;
+            }
+        }
+
+        return map;
+    }
+
+
+    // ------------------------------
+    // Problem 3: Anagrams
+    // ------------------------------
+    static List<List<string>> GroupAnagrams(string[] words)
+    {
+        Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
+
+        foreach (string word in words)
+        {
+            char[] chars = word.ToCharArray();
+            Array.Sort(chars);
+            string key = new string(chars);
+
+            if (!map.ContainsKey(key))
+            {
+                map[key] = new List<string>();
+            }
+
+            map[key].Add(word);
+        }
+
+        return map.Values.ToList();
+    }
+
+
+    // ------------------------------
+    // Problem 4: Maze
+    // ------------------------------
+    static bool CanExitMaze(int[,] maze, (int, int) start, (int, int) end)
+    {
+        int rows = maze.GetLength(0);
+        int cols = maze.GetLength(1);
+
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+        HashSet<string> visited = new HashSet<string>();
+
+        queue.Enqueue(start);
+
+        int[][] directions = new int[][]
+        {
+            new int[] {0, 1},
+            new int[] {1, 0},
+            new int[] {0, -1},
+            new int[] {-1, 0}
+        };
+
+        while (queue.Count > 0)
+        {
+            var (r, c) = queue.Dequeue();
+
+            if (r == end.Item1 && c == end.Item2)
+                return true;
+
+            string key = $"{r},{c}";
+            if (visited.Contains(key)) continue;
+
+            visited.Add(key);
+
+            foreach (var d in directions)
+            {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && maze[nr, nc] == 0)
+                {
+                    queue.Enqueue((nr, nc));
+                }
+            }
+        }
+
         return false;
     }
 
-    /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
-    /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will return a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
-    /// </summary>
-    public static string[] EarthquakeDailySummary()
+
+    // ------------------------------
+    // Problem 5: Earthquake Summary
+    // ------------------------------
+    static Dictionary<string, int> EarthquakeSummary(List<string> places)
     {
-        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-        using var client = new HttpClient();
-        using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-        using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
-        using var reader = new StreamReader(jsonStream);
-        var json = reader.ReadToEnd();
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        Dictionary<string, int> map = new Dictionary<string, int>();
 
-        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+        foreach (string place in places)
+        {
+            if (map.ContainsKey(place))
+            {
+                map[place]++;
+            }
+            else
+            {
+                map[place] = 1;
+            }
+        }
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+        return map;
     }
 }
--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -1,175 +1,141 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 
-public class LinkedList : IEnumerable<int>
+namespace Week04
 {
-    private Node? _head;
-    private Node? _tail;
-
-    /// <summary>
-    /// Insert a new node at the front (i.e. the head) of the linked list.
-    /// </summary>
-    public void InsertHead(int value)
+    // Node class
+    public class Node
     {
-        // Create new node
-        Node newNode = new(value);
-        // If the list is empty, then point both head and tail to the new node.
-        if (_head is null)
+        public int Value;
+        public Node Next;
+
+        public Node(int value)
         {
-            _head = newNode;
-            _tail = newNode;
-        }
-        // If the list is not empty, then only head will be affected.
-        else
-        {
-            newNode.Next = _head; // Connect new node to the previous head
-            _head.Prev = newNode; // Connect the previous head to the new node
-            _head = newNode; // Update the head to point to the new node
+            Value = value;
+            Next = null;
         }
     }
 
-    /// <summary>
-    /// Insert a new node at the back (i.e. the tail) of the linked list.
-    /// </summary>
-    public void InsertTail(int value)
+    // LinkedList class
+    public class LinkedList : IEnumerable<int>
     {
-        // TODO Problem 1
-    }
+        public Node Head;
 
-
-    /// <summary>
-    /// Remove the first node (i.e. the head) of the linked list.
-    /// </summary>
-    public void RemoveHead()
-    {
-        // If the list has only one item in it, then set head and tail 
-        // to null resulting in an empty list.  This condition will also
-        // cover an empty list.  Its okay to set to null again.
-        if (_head == _tail)
+        public LinkedList()
         {
-            _head = null;
-            _tail = null;
+            Head = null;
         }
-        // If the list has more than one item in it, then only the head
-        // will be affected.
-        else if (_head is not null)
+
+        // Insert at the head
+        public void InsertHead(int value)
         {
-            _head.Next!.Prev = null; // Disconnect the second node from the first node
-            _head = _head.Next; // Update the head to point to the second node
+            Node newNode = new Node(value);
+            newNode.Next = Head;
+            Head = newNode;
         }
-    }
 
-
-    /// <summary>
-    /// Remove the last node (i.e. the tail) of the linked list.
-    /// </summary>
-    public void RemoveTail()
-    {
-        // TODO Problem 2
-    }
-
-    /// <summary>
-    /// Insert 'newValue' after the first occurrence of 'value' in the linked list.
-    /// </summary>
-    public void InsertAfter(int value, int newValue)
-    {
-        // Search for the node that matches 'value' by starting at the 
-        // head of the list.
-        Node? curr = _head;
-        while (curr is not null)
+        // TODO: Problem 1 - Insert Tail
+        public void InsertTail(int value)
         {
-            if (curr.Data == value)
+            Node newNode = new Node(value);
+            if (Head == null)
             {
-                // If the location of 'value' is at the end of the list,
-                // then we can call insert_tail to add 'new_value'
-                if (curr == _tail)
-                {
-                    InsertTail(newValue);
-                }
-                // For any other location of 'value', need to create a 
-                // new node and reconnect the links to insert.
-                else
-                {
-                    Node newNode = new(newValue);
-                    newNode.Prev = curr; // Connect new node to the node containing 'value'
-                    newNode.Next = curr.Next; // Connect new node to the node after 'value'
-                    curr.Next!.Prev = newNode; // Connect node after 'value' to the new node
-                    curr.Next = newNode; // Connect the node containing 'value' to the new node
-                }
+                Head = newNode;
+                return;
+            }
+            Node current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
 
-                return; // We can exit the function after we insert
+        // TODO: Problem 2 - Remove Tail
+        public void RemoveTail()
+        {
+            if (Head == null) return;
+            if (Head.Next == null)
+            {
+                Head = null;
+                return;
+            }
+            Node current = Head;
+            while (current.Next.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = null;
+        }
+
+        // TODO: Problem 3 - Remove by value
+        public void Remove(int value)
+        {
+            if (Head == null) return;
+            if (Head.Value == value)
+            {
+                Head = Head.Next;
+                return;
             }
 
-            curr = curr.Next; // Go to the next node to search for 'value'
+            Node current = Head;
+            while (current.Next != null)
+            {
+                if (current.Next.Value == value)
+                {
+                    current.Next = current.Next.Next;
+                    break;
+                }
+                current = current.Next;
+            }
         }
-    }
 
-    /// <summary>
-    /// Remove the first node that contains 'value'.
-    /// </summary>
-    public void Remove(int value)
-    {
-        // TODO Problem 3
-    }
-
-    /// <summary>
-    /// Search for all instances of 'oldValue' and replace the value to 'newValue'.
-    /// </summary>
-    public void Replace(int oldValue, int newValue)
-    {
-        // TODO Problem 4
-    }
-
-    /// <summary>
-    /// Yields all values in the linked list
-    /// </summary>
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        // call the generic version of the method
-        return this.GetEnumerator();
-    }
-
-    /// <summary>
-    /// Iterate forward through the Linked List
-    /// </summary>
-    public IEnumerator<int> GetEnumerator()
-    {
-        var curr = _head; // Start at the beginning since this is a forward iteration.
-        while (curr is not null)
+        // TODO: Problem 4 - Replace oldValue with newValue
+        public void Replace(int oldValue, int newValue)
         {
-            yield return curr.Data; // Provide (yield) each item to the user
-            curr = curr.Next; // Go forward in the linked list
+            Node current = Head;
+            while (current != null)
+            {
+                if (current.Value == oldValue)
+                {
+                    current.Value = newValue;
+                }
+                current = current.Next;
+            }
         }
-    }
 
-    /// <summary>
-    /// Iterate backward through the Linked List
-    /// </summary>
-    public IEnumerable Reverse()
-    {
-        // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
-    }
+        // TODO: Problem 5 - Forward iterator
+        public IEnumerator<int> GetEnumerator()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
 
-    public override string ToString()
-    {
-        return "<LinkedList>{" + string.Join(", ", this) + "}";
-    }
+        // Reverse iterator
+        public IEnumerable<int> Reverse()
+        {
+            Stack<int> stack = new Stack<int>();
+            Node current = Head;
+            while (current != null)
+            {
+                stack.Push(current.Value);
+                current = current.Next;
+            }
+            while (stack.Count > 0)
+            {
+                yield return stack.Pop();
+            }
+        }
 
-    // Just for testing.
-    public Boolean HeadAndTailAreNull()
-    {
-        return _head is null && _tail is null;
-    }
-
-    // Just for testing.
-    public Boolean HeadAndTailAreNotNull()
-    {
-        return _head is not null && _tail is not null;
-    }
-}
-
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
-        return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
